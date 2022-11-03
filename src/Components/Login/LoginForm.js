9f8aa17711b217d80e62d45f2b96e1c683f46124
Vitosnatios <vitosdeveloper@gmail.com>
2022-11-03 08:15:42 -0300

    api fetch

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
+import { TOKEN_POST, USER_GET } from '../../api';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const onSubmit = (e) => {
+  const getUser = async (token) => {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const logar = async () => {
-      const res = await fetch(
-        'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-        {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: username.value,
-            password: password.value,
-          }),
-        }
-      );
-      const json = await res.json();
-      console.log(json);
-    };
-
     if (username.validate() && password.validate()) {
-      logar();
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const res = await fetch(url, options);
+      const json = await res.json();
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   };
 
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) getUser(token);
+  }, []);
+
   return (
-    <section onSubmit={onSubmit}>
+    <section onSubmit={handleSubmit}>
       <h1>Login</h1>
       <form>
         <Input label='Usuário' type='text' name='username' {...username} />
