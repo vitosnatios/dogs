0db58822bdab19b2bf52cf2f95deaf72215010d0
Vitosnatios <vitosdeveloper@gmail.com>
2022-11-05 15:42:37 -0300

    image skeleton

--- src/Components/Photo/PhotoDelete.js
+++ src/Components/Photo/PhotoDelete.js
@@ -10,7 +10,7 @@ const PhotoDelete = ({ id }) => {
     const confirm = window.confirm('Tem certeza que deseja deletar?');
     if (confirm) {
       const { url, options } = PHOTO_DELETE(id);
-      const response = await request(url, options);
+      const { response } = await request(url, options);
       if (response.ok) window.location.reload();
     }
   };
