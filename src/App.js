8e23611b808ae352f966eaa53ba437480090b7c8
Vitosnatios <vitosdeveloper@gmail.com>
2022-11-06 09:37:35 -0300

    erro 404 e proptypes no feed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Header from './Components/Header';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound';
 import Photo from './Components/Photo/Photo';
 import User from './Components/User/User';
+import UserProfile from './Components/User/UserProfile';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -27,6 +29,8 @@ function App() {
               }
             />
             <Route path='foto/:id' element={<Photo />} />
+            <Route path='perfil/:user' element={<UserProfile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
